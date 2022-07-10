c5ec18e0a4b42b83d729b6d4d08afab425f2f6f0
mailajaykumar <58897008+mailajaykumar@users.noreply.github.com>
2022-07-10 15:47:27 +0800

    initial commit for project

---
+++ src/main/java/com/jenkins/code1/Code1Application.java
@@ -0,0 +1,19 @@
+package com.jenkins.code1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Code1Application {
+
+private static final Logger logger = LoggerFactory.getLogger(Code1Application.class);
+
+	public static void main(String[] args) {
+		logger.info("Applicatiuon started");
+		SpringApplication.run(Code1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkins/code1/Code1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.code1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Code1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
